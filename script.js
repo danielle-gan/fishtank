deca64c9f11aed0a8cff35205ee3fcff1e2d5af2
Danielle Gan <DGan@data-mail.com>
2023-05-16 15:54:14 -0400

    added a shark, still can't figure out why transform scaleX doesn't work, but it's okay

--- script.js
+++
@@ -1,7 +0,0 @@
-const fishElement = document.querySelector('.fish1');
-
-function getRandomMovement() {
-  return Math.random() > 0.5 ? '80vw' : '-80vw';
-}
-
-fishElement.style.setProperty('--random-movement', getRandomMovement());
